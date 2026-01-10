3cf9c07e87ae49a58f014c7d5764a88893dd6e66
TheViking816 <adrianlujan94@yahoo.es>
2026-01-10 13:35:27 +0100

    Force PWA cache refresh

--- vite.config.ts
+++ vite.config.ts
@@ -45,6 +45,9 @@ export default defineConfig(({ mode }) => {
           },
           workbox: {
             globPatterns: ['**/*.{js,css,html,svg,png,webmanifest,ico}'],
+            cleanupOutdatedCaches: true,
+            clientsClaim: true,
+            skipWaiting: true,
           },
         }),
       ],
